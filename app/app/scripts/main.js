29768ce17aff9a596964ab6ce9cc70afa3a2ae16
chrisUsick <christopher.usick@gmail.com>
2015-07-31 11:32:46 -0500

    beginning search script

--- app/app/scripts/main.js
+++ app/app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+// console.log('\'Allo \'Allo!');
+
+
+var input = $('#search');
+
+input.keyup(function (ev) {
+	// console.log(ev);
+	// 1. split input into tokens
+	// 2. query like so to get matching scholarships
+	// $('.scholarship[data-tags*="token[0]"], .scholarship[data-tags*="token[1]"]')
+});
